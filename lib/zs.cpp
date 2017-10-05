0fbefc08459c13f69fb8b3b8ec4e498ecc403e79
Victor Lee <lzs_1993@qq.com>
2017-10-05 11:02:29 +0800

    开始搭建CTest

---
+++ lib/zs.cpp
@@ -0,0 +1,6 @@
+#include "zs.h"
+
+int zs(int i)
+{
+    return i + 1;
+}
--- src/hello_world/hello_world.cpp
+++ src/hello_world/hello_world.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 
+#include "zs.h"
+
 using namespace std;
 
-inline int zs(int i){
-	return i+1;
-}
-
-int main(){
+int main()
+{
 	int num;
-	num=1;
 	cin >> num;
 	cout << zs(num) << endl;
 	cout << "num = " << num << endl;
 	cout << "Hello world!" << endl;
 
-
-	
 	return 0;
 }
