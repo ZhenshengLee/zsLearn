28ca65d7ba610c9c5f5f97fec60dc81f13b7837d
Victor Lee <lzs_1993@qq.com>
2017-10-14 01:20:32 +0800

    添加cpplint，根据提示简单修改helloworldcppfile

--- src/hello_world/hello_world.cpp
+++ src/hello_world/hello_world.cpp
@@ -7,20 +7,20 @@ using namespace std;
 
 int main()
 {
-	LOG_INIT("HelloWorld");
-	LOG_CONFIG();
-	int radius;
-	cout << "Please input the radius of the circle" << endl;
-	// cin >> radius;
-	radius =3;
-	if (0>radius)
-	{
-		cout << "error!" << endl;
-		return -1;
-	}
-	zs zs_circle(radius);
-	LOG_DEBUG("Hello, world");
-	cout << "the area of zs_circle is " << zs_circle.getArea() << endl;
+    LOG_INIT("HelloWorld");
+    LOG_CONFIG();
+    int radius;
+    cout << "Please input the radius of the circle" << endl;
+    // cin >> radius;
+    radius = 3;
+    if (0 > radius)
+    {
+        cout << "error!" << endl;
+        return -1;
+    }
+    zs zs_circle(radius);
+    LOG_DEBUG("Hello, world");
+    cout << "the area of zs_circle is " << zs_circle.getArea() << endl;
 
-	return 0;
+    return 0;
 }
