ec629cb384ce3cd8591def85eb9f45b40c71d7d6
Victor Lee <lzs_1993@qq.com>
2018-03-25 11:43:22 +0800

    添加Fixture的单元测试

--- src/helloWorld/helloWorldTest.cpp
+++ src/helloWorld/helloWorldTest.cpp
@@ -9,3 +9,20 @@ TEST(hello_world, integer)
     double actual = helloWorld_circle.getArea();
     ASSERT_EQ(expected, actual);
 }
+
+class helloWorldTest : public ::testing::Test
+{
+  protected:
+    virtual void SetUp() {}
+
+    virtual void TearDown() {}
+
+};
+
+TEST_F(helloWorldTest, helloWorldTest_integer_returnEQ)
+{
+    helloWorld helloWorld_circle(1);
+    double expected = 9.8596;
+    double actual = helloWorld_circle.getArea();
+    ASSERT_EQ(expected, actual);
+}
